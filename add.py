2712d1d3b79baf3078d5099f83d15128bd3a552d
waleed.ali <waled.ali1888309@gmail.com>
2023-02-05 16:42:39 +0200

    first commit

---
+++ add.py
@@ -0,0 +1,4 @@
+x= "WALEED , ALI"
+z = "hassen"
+y= x.join(z)
+print(y)
---
+++ waleed.py
@@ -0,0 +1,6 @@
+x = str(input("please enter sentence :"))
+y = input("please enter char :")
+if y in x :
+    print(y)
+else :
+    print("not allaw")
